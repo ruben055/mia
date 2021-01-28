67b007c9ee71976e06a417f0b09e04796343e9da
Ruben Mual Siregar <63962557+ruben055@users.noreply.github.com>
2021-01-28 20:36:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,103 +1,284 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Mia</title>
-<style type="text/css">
-body {
-background-image: url("coba1.gif");
-}
+<!doctype html>
+<html lang="en">
 
-.animasi-teks {
-  font-size: 20px;
-  width: 100%;
-  white-space:nowrap;
-  overflow:hidden;
-  -webkit-animation: typing 5s steps(70, end);
-  animation: animasi-ketik 5s steps(70, end);
-}
+  <head>
+    <title>Ruben Portofolio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@keyframes animasi-ketik{
-  from { width: 0; }
-}
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,700" rel="stylesheet">
 
-@-webkit-keyframes animasi-ketik{
-  from { width: 0; }
-}
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-</style>
-</head>
-<body>
- 
-	<style>
-	h1,h2,p,a{
-		font-family: sans-serif;
-		font-weight: normal;
-	}
- 
-	.jam-digital-malasngoding {
-		overflow: hidden;
-		width: 330px;
-		margin: 20px auto;
-		border: 5px solid #efefef;
-	}
-	.kotak{
-		float: left;
-		width: 110px;
-		height: 100px;
-		background-color: #262626;
-	}
-	.jam-digital-malasngoding p {
-		color: #fff;
-		font-size: 36px;
-		text-align: center;
-		margin-top: 30px;
-	}
- 
- 
-</style>
- <br>
- <br>
- <br>
- <br>
- <br>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
 
 
-<center>
-<div class="animasi-teks">
-	<h1><font face="tipe_font_face" color="white">Haiii, Miaaa :) </font></h1>
-</div>
-</center>
- 
-<div class="jam-digital-malasngoding">
-	<div class="kotak">
-		<p id="jam"></p>
-	</div>
-	<div class="kotak">
-		<p id="menit"></p>
-	</div>
-	<div class="kotak">
-		<p id="detik"></p>
-	</div>
-</div>
- <center>
- 	<h2><font face="tipe_font_face" color="white">Semprotulation Yaaaaa!</font></h2>
- 	<h2><font face="tipe_font_face" color="white">Gas Keun Teruss - Gk acci Mundur </font></h2>
- 	<h2><font face="tipe_font_face" color="white"> -Ben :v </font></h2>
- 
 
-</center>
+      <header class="site-navbar site-navbar-target" role="banner">
 
-<script>
-	window.setTimeout("waktu()", 1000);
- 
-	function waktu() {
-		var waktu = new Date();
-		setTimeout("waktu()", 1000);
-		document.getElementById("jam").innerHTML = waktu.getHours();
-		document.getElementById("menit").innerHTML = waktu.getMinutes();
-		document.getElementById("detik").innerHTML = waktu.getSeconds();
-	}
+        <div class="container">
+          <div class="row align-items-center position-relative">
 
-</script>
-</body>
+            <div class="col-3 ">
+              <div class="site-logo">
+                <a href="index.html" class="font-weight-bold">Portofolio</a>
+              </div>
+            </div>
+
+            <div class="col-9  text-right">
+              
+
+              <span class="d-inline-block d-lg-block"><a href="#" class="text-white site-menu-toggle js-menu-toggle py-5 text-white"><span class="icon-menu h3 text-white"></span></a></span>
+
+              
+
+              <nav class="site-navigation text-right ml-auto d-none d-lg-none" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav ml-auto ">
+                  <li class="active"><a href="index.html" class="nav-link">Home</a></li>
+                </ul>
+              </nav>
+            </div>
+
+            
+          </div>
+        </div>
+
+      </header>
+
+    <div class="ftco-blocks-cover-1">
+      <div class="site-section-cover overlay" style="background-image: url('images/hero_1.jpg')">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-7 text-center">
+              <h1 class="mb-4 text-white">Hi!! Welcome to my portfolio.</h1>
+              <p class="mb-4">Click the button below to see my CV</p>
+              <p><a href="https://drive.google.com/file/d/1j3B_aVhuoSmwb5w61T0wQCVKqf7s0ydK/view?usp=sharing" class="btn btn-primary btn-outline-white py-3 px-5">My CV</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4">
+            <h2 class="h4 mb-4">About Me</h2>
+            <p>Hi! My Name is Ruben Mual Siregar , </p>
+            <p>I was born in Porsea,May 5th 2000.</p>
+            <p>Now, I'm still student Computer Technology in Institut Teknologi Del. Really enjoying sports and Playing Games. It's one of my way to get inspirations</p>
+            <p>I really hope you give me opportunity to join your company or stduio to raise my experience. Thank You</p>
+             </div>
+          <div class="col-md-4">
+            <img src="foto.jpg" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-md-4">
+            
+            <h2 class="h4 mb-4">Personal skills</h2>
+
+            <div class="progress-wrap mb-4">
+              <div class="d-flex">
+                <span>FrontEnd</span>
+                <span class="ml-auto">75%</span>
+              </div>
+              <div class="progress rounded-0" style="height: 7px;">
+                <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress-wrap mb-4">
+              <div class="d-flex">
+                <span>BackEnd</span>
+                <span class="ml-auto">65%</span>
+              </div>
+              <div class="progress rounded-0" style="height: 7px;">
+                <div class="progress-bar" role="progressbar" style="width: 65%;" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress-wrap mb-4">
+              <div class="d-flex">
+                <span>Networking</span>
+                <span class="ml-auto">60%</span>
+              </div>
+              <div class="progress rounded-0" style="height: 7px;">
+                <div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="93" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress-wrap mb-4">
+              <div class="d-flex">
+                <span>Programming</span>
+                <span class="ml-auto">70%</span>
+              </div>
+              <div class="progress rounded-0" style="height: 7px;">
+                <div class="progress-bar" role="progressbar" style="width: 70%;" aria-valuenow="83" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+               <div class="progress-wrap mb-4">
+              <div class="d-flex">
+                <span>Creativity</span>
+                <span class="ml-auto">85%</span>
+              </div>
+              <div class="progress rounded-0" style="height: 7px;">
+                <div class="progress-bar" role="progressbar" style="width: 85%;" aria-valuenow="83" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+   <div class="progress-wrap mb-4">
+              <div class="d-flex">
+                <span>Public Speaking</span>
+                <span class="ml-auto">55%</span>
+              </div>
+              <div class="progress rounded-0" style="height: 7px;">
+                <div class="progress-bar" role="progressbar" style="width: 55%;" aria-valuenow="83" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-7 mx-auto text-center">
+            <h2 class="heading-29190">Projects Undertaken During Study</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-lg-3">
+            <div class="service-29128 text-center">
+              <span class="d-block wrap-icon">
+                <span class="icon-desktop_mac"></span>
+              </span>
+              <h3>Static Website of Rising Vegetable</h3>
+              <p>This project aims to create a static web to Help Farmer sell their natural product.This Web using HTML,CSS,and Javascipt.</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="service-29128 text-center">
+              <span class="d-block wrap-icon">
+                <span class="icon-desktop_mac"></span>
+              </span>
+              <h3>Grup Chat from Java Language</h3>
+              <p>This project was made as a requirement to pass the course Program Berorientasi (PBO) using Java. </p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="service-29128 text-center">
+              <span class="d-block wrap-icon">
+                <span class="icon-desktop_mac"></span>
+              </span>
+              <h3>Dynamic web of TobaShip </h3>
+              <p>This project uses PHP and MySql. This website was built to help people to book boat ticket online. </p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="service-29128 text-center">
+              <span class="d-block wrap-icon">
+                <span class="icon-desktop_mac"></span>
+              </span>
+              <h3>Tracking Students With Arduino</h3>
+              <p>This project focuses on students who do not go to college so a position tracker is made for the students. This project was built with Arduino and GPS Module</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <h2 class="footer-heading mb-3">About Me</h2>
+                <p>“Gagal itu makanan sehari-hari. Itu biasa, yang penting bagimana (kamu) menyikapinya. Evaluasi. Bangkit. Gagal lagi? Bangkit lagi!” – Chairul Tanjung</p>
+          </div>
+          <div class="col-lg-8 ml-auto">
+            <div class="row">
+              <div class="col-lg-6 ml-auto">
+                <h2 class="footer-heading mb-4">Quick Links</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <h2 class="footer-heading mb-4">Connect</h2>
+                <div class="social_29128 white mb-5">
+                  <a href="https://web.facebook.com/ruben.siregar.16"><span class="icon-facebook"></span></a>  
+                  <a href="https://www.linkedin.com/in/ruben-siregar-812623197/"><span class="icon-linkedin"></span></a>  
+                 </div>
+               
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <div class="border-top pt-5">
+              <p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+  </body>
+
 </html>
+
